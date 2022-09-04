8c0bf12cbd6826774d77d516ce7e22af4f974476
MotaNT <natasha.silva4@fatec.sp.gov.br>
2022-09-04 18:00:19 -0300

    pegando palavras pelo coletor

--- src/br/com/forcaita/domain/BancoDePalavras.java
+++ src/br/com/forcaita/domain/BancoDePalavras.java
@@ -1,10 +1,15 @@
 package br.com.forcaita.domain;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+
+import br.com.forcaita.domain.util.ColetorPalavraWEB;
 
 
 
@@ -17,6 +22,10 @@ public class BancoDePalavras {
 		this.indice = 0;
 		this.carregaPalavrasTXT();
 		this.embaralhaPalavras();
+	}
+	
+	public int pegarQuantidadePalavras() {
+		return this.palavras.size();
 	}
 	
 	public String pegaPalavraAleatoria() {
@@ -34,12 +43,38 @@ public class BancoDePalavras {
 	private void carregaPalavrasTXT(){
 		String filePath = "/home/aluna/git/Forca-ITA/dados/animais_palavras.txt";
 		try {
-			Scanner scan = new Scanner(new File(filePath));
-			while(scan.hasNext()) {
-				this.palavras.add(scan.next());	
-							}	
+			File arquivo = new File(filePath);
+			if(!arquivo.exists()) {
+				criarArquivo(arquivo);
+			} 
+			
+			lerArquivo(filePath);
+							
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void criarArquivo(File arquivo) throws IOException {
+		ColetorPalavraWEB coletorPalavras = new ColetorPalavraWEB();
+		
+		FileWriter escritor = new FileWriter(arquivo);
+		PrintWriter gravarArq = new PrintWriter(escritor);
+		ArrayList<String> palavrascopia = coletorPalavras.pegaPalavras();
+		for(String palavra: palavrascopia){
+			gravarArq.println(palavrascopia);
+		}
+		arquivo.createNewFile();
+		gravarArq.close();
+	}
+	
+	private void lerArquivo(String arquivo) throws FileNotFoundException {
+		Scanner scan = new Scanner(new File(arquivo));
+		while(scan.hasNext()) {
+			this.palavras.add(scan.next());
 		}
 	}
 }
--- src/br/com/forcaita/domain/util/ColetorPalavraWEB.java
+++ src/br/com/forcaita/domain/util/ColetorPalavraWEB.java
@@ -1,12 +1,55 @@
 package br.com.forcaita.domain.util;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class ColetorPalavraWEB {
 
-	private static final String URL = "https://www.dicio.com.br/";
+	private static final String URL = "https://www.dicio.com.br/animais-de-a-a-z/";
+	private ArrayList<String> palavras = new ArrayList<String>();
 	
-	//js-pl-aleatoria
-	//id="js-btn-sortear"
-
+	public ColetorPalavraWEB() {
+		this.preencherArrayPalavras();
+		
+	}
 	
-
+	
+	public ArrayList<String> pegaPalavras() {
+		ArrayList<String> copiaPalavras = new ArrayList<String>();
+		for (String palavra: this.palavras){
+			copiaPalavras.add(palavra);
+		}
+		return copiaPalavras;
+	}
+	
+	private Elements pegarListItens() {
+		Document doc;
+		try {
+			doc = Jsoup.connect(URL).get();
+			Elements listItens = (doc.getElementById("contentArticle").select("li").not(".item"));
+			return listItens;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;	
+	}
+	
+	private void preencherArrayPalavras(){
+		for (Element li : pegarListItens()) {
+				this.palavras.add(this.limparPalavraLI(li));		
+		}
+	}
+	
+	private String limparPalavraLI(Element li) {
+		String palavra;
+		palavra = li.toString();
+		palavra = palavra.substring(4, palavra.length()-6);
+		return palavra;
+	}
 }
